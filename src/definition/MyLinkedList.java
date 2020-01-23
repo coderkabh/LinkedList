11e1e7d1a57e35863a77a8bd166a15d7fa9b122d
kanishk <kanishkbharadwaj333@gmail.com>
2020-01-23 11:03:30 +0530

    create the constructor for data and node and make getter methods

--- src/definition/MyLinkedList.java
+++ src/definition/MyLinkedList.java
@@ -17,6 +17,19 @@ public class MyLinkedList<E> implements LinkedListADT<E> {
         public Node(E data) {
             this.data = data;
         }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
     }
 
     @Override
